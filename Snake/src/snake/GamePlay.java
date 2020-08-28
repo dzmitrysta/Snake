1a822f17fd24b9bdf1c148217fd8379a1cf5901b
dzmitrysta <zmitrukg@yandex.ru>
2020-08-27 22:00:52 -0500

    Added functionality

--- Snake/src/snake/GamePlay.java
+++ Snake/src/snake/GamePlay.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
@@ -34,6 +35,22 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 	
 	private int moves = 0;
 	private int score = 0;
+	
+	//Fruit Position
+	
+		private int [] fruitXpos = {25,50,75,100,125,150,175,200,225,250,275,300,325,250,375,400,425,450,475,500,525,550,575,600,
+				625,650,675,700,725,750,775,800,825,850};
+		
+		private int [] fruitYpos = {75,100,125,150,175,200,225,250,275,300,325,250,375,400,425,450,475,500,525,550,575,600,
+				625};
+
+		
+		private ImageIcon fruitimage;
+		
+		private Random random = new Random();
+		
+		private int xpos = random.nextInt(34);
+		private int ypos = random.nextInt(23);
 	
 	
 	private ImageIcon titleImage;
@@ -101,6 +118,18 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 				}
 		}
 		
+		//Part5
+				fruitimage = new ImageIcon("fruit.png");
+				if(fruitXpos[xpos] == snakeXLenght[0] && fruitYpos[ypos] == snakeYLenght[0])
+				{
+					score= score+5;
+					lengthOfSnake++;
+					xpos = random.nextInt(34);
+					ypos = random.nextInt(23);
+				}
+				
+				fruitimage.paintIcon(this,g,fruitXpos[xpos], fruitYpos[ypos]);
+		
 		g.dispose();
 		
 	}
@@ -109,6 +138,108 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		timer.restart();
+		if(right) 
+		{
+			for(int n = lengthOfSnake-1; n>=0;n--)
+			{
+				snakeYLenght[n+1] = snakeYLenght[n];
+			}
+			for(int n = lengthOfSnake; n>=0; n--) 
+			{
+				if (n==0) 
+				{
+					snakeXLenght[n] = snakeXLenght[n]+25;
+				}
+				else 
+				{
+					snakeXLenght[n] = snakeXLenght[n-1];
+				}
+				if(snakeXLenght[n] >850)
+				{
+					snakeXLenght[n] = 25;
+				}
+				
+			}
+			repaint();
+		}
+		
+		if(left) 
+		{
+			for(int n = lengthOfSnake-1; n>=0;n--)
+			{
+				snakeYLenght[n+1] = snakeYLenght[n];
+			}
+			for(int n = lengthOfSnake; n>=0; n--) 
+			{
+				if (n==0) 
+				{
+					snakeXLenght[n] = snakeXLenght[n]-25;
+				}
+				else 
+				{
+					snakeXLenght[n] = snakeXLenght[n-1];
+				}
+				if(snakeXLenght[n] < 25)
+				{
+					snakeXLenght[n] = 850;
+				}
+				
+			}
+			repaint();			
+			
+		}
+		if(up) 
+		{
+			for(int n = lengthOfSnake-1; n>=0;n--)
+			{
+				snakeXLenght[n+1] = snakeXLenght[n];
+			}
+			for(int n = lengthOfSnake; n>=0; n--) 
+			{
+				if (n==0) 
+				{
+					snakeYLenght[n] = snakeYLenght[n]-25;
+				}
+				else 
+				{
+					snakeYLenght[n] = snakeYLenght[n-1];
+				}
+				if(snakeYLenght[n] < 75)
+				{
+					snakeYLenght[n] = 625;
+				}
+				
+			}
+			repaint();
+			
+		}
+		if(down) 
+		{
+			for(int n = lengthOfSnake-1; n>=0;n--)
+			{
+				snakeXLenght[n+1] = snakeXLenght[n];
+			}
+			for(int n = lengthOfSnake; n>=0; n--) 
+			{
+				if (n==0) 
+				{
+					snakeYLenght[n] = snakeYLenght[n]+25;
+				}
+				else 
+				{
+					snakeYLenght[n] = snakeYLenght[n-1];
+				}
+				if(snakeYLenght[n] > 625)
+				{
+					snakeYLenght[n] = 75;
+				}
+				
+			}
+			
+			repaint();
+		}
+		
 		
 	}
 
@@ -122,7 +253,91 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+
+		if(e.getKeyCode() == KeyEvent.VK_ENTER)
+		{
+			moves = 0;
+			score = 0;
+			lengthOfSnake=3;
+			repaint();
+		}
+		
+		
+		//Key response
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT)
+		{
+			moves++;
+			right = true;
+			if(!left) {
+				right = true;
+			}
+			else
+			{
+				right = false;
+				left = true;
+			}
+			
+			up = false;
+			down = false;
+			
+			
+		}
+		if(e.getKeyCode() == KeyEvent.VK_LEFT)
+		{
+			moves++;
+			left = true;
+			if(!right) {
+				left = true;
+			}
+			else
+			{
+				left = false;
+				right = true;
+			}
+			
+			up = false;
+			down = false;
+			
+			
+		}
+		if(e.getKeyCode() == KeyEvent.VK_UP)
+		{
+			moves++;
+			up = true;
+			if(!down) {
+				up = true;
+			}
+			else
+			{
+				up = false;
+				down = true;
+			}
+			
+			left = false;
+			right = false;
+			
+			
+		}
+		if(e.getKeyCode() == KeyEvent.VK_DOWN)
+		{
+			moves++;
+			down = true;
+			if(!up) {
+				down = true;
+			}
+			else
+			{
+				up = true;
+				down = false;
+				
+			}
+			
+			left = false;
+			right = false;
+			
+			
+		}
+		
 		
 	}
 
